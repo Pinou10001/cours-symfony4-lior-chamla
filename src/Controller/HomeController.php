27b55467935c1579d3084233d1482c87314e5214
Martin Soenen <martin.soenen@outlook.com>
2020-04-08 19:15:23 +0200

    4 - Comprendre Twig : les structures de contrôle (conditions et boucles)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,10 +11,14 @@ class HomeController extends Controller {
      * @Route("/", name="homepage")
      */
     public function home(){
+        $prenoms = ["Martin" => 20, "Inès" => 19, "Lior" => 31, "Valérie" => 52, "Christophe" => 58];
+
         return $this->render(
             'home.html.twig',
             [
-                'title' => "Bonjour à tous"
+                'title' => "Bonjour à tous",
+                'age' => 20,
+                'tableau' => $prenoms
             ]
         );
     }
