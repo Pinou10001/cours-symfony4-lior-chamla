8ccd1f683324c52ccaebffbfeea580e8a0a4163e
Martin Soenen <martin.soenen@outlook.com>
2020-04-08 19:52:39 +0200

    7 - Routes paramétrées & routes multiples : des adresses dynamique porteuses de sens

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller {
+
+    /**
+     * @Route("/hello/{prenom}/{age}", name="hello")
+     * @Route("/hello")
+     * Montre la page qui dit bonjour
+     */
+    public function hello($prenom = "anonyme", $age = 0) {
+        return new Response("Bonjour " . $prenom . ", vous avez " . $age . " ans.");
+    }
 
     /**
      * @Route("/", name="homepage")
