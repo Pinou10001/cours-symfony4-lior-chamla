341264c00147f80a0c222cc835f38386b8523fa0
Martin Soenen <martin.soenen@outlook.com>
2020-04-10 17:05:39 +0200

    18 - Comprendre Doctrine : interagir avec le cycle de vie des Entités

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
-use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -13,15 +12,11 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr-FR');
-        $slugify = new Slugify();
 
         for ($i = 1; $i <= 30; $i++) {
             $ad = new Ad();
 
-            $title =$faker->sentence();
-
-            $ad->setTitle($title)
-                ->setSlug($slugify->slugify($title))
+            $ad->setTitle($faker->sentence())
                 ->setCoverImage($faker->imageUrl(1000,350))
                 ->setIntroduction($faker->paragraph(2))
                 ->setContent('<p>' . join('</p><p>' , $faker->paragraphs(5)) . '</p>')
