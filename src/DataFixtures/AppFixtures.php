597f62eed90ca625ac6129100d58ff69034c677c
Martin Soenen <martin.soenen@outlook.com>
2020-04-10 17:18:10 +0200

    20 - Enrichir la Fixture pour créer des images

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -22,6 +23,16 @@ class AppFixtures extends Fixture
                 ->setContent('<p>' . join('</p><p>' , $faker->paragraphs(5)) . '</p>')
                 ->setPrice(mt_rand(40, 200))
                 ->setRooms(mt_rand(1, 5));
+
+            for ($j = 1; $j <= mt_rand(2, 5); $j++) {
+                $image = new Image();
+
+                $image->setUrl($faker->imageUrl())
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+
+                $manager->persist($image);
+            }
 
             $manager->persist($ad);
         }
