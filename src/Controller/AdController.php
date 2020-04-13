f65cafe1477b691ab83984cb0dc5b89529c9e126
Martin Soenen <martin.soenen@outlook.com>
2020-04-13 18:14:00 +0200

    24 - Récupération d'une annonce avec son slug grâce au Repository

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Repository\AdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdController extends AbstractController
@@ -18,6 +19,20 @@ class AdController extends AbstractController
 
         return $this->render('ad/index.html.twig', [
             'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une annonce
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo) {
+        $ad = $repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
         ]);
     }
 }
