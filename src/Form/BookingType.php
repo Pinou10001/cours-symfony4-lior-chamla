2d5b801ef24f2c3e33f1c2a563eac15f65ef1045
Martin Soenen <martin.soenen@outlook.com>
2020-05-13 16:40:20 +0200

    97 - Création du formulaire de réservation

---
+++ src/Form/BookingType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('startDate', DateType::class, $this->getConfiguration("Date d'arrivée", "La date à laquelle vous comptez arriver"))
+            ->add('endDate', DateType::class, $this->getConfiguration("Date de départ", "Date à laquelle vous quittez les lieux"))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
