0ffd39c1a2bd7189aeab5f84affd1a9caec4bb43
Martin Soenen <martin.soenen@outlook.com>
2020-05-13 17:06:40 +0200

    99 - Ajout d'un champ de commentaire sur les réservations

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +16,7 @@ class BookingType extends ApplicationType
         $builder
             ->add('startDate', DateType::class, $this->getConfiguration("Date d'arrivée", "La date à laquelle vous comptez arriver", ["widget" => "single_text"]))
             ->add('endDate', DateType::class, $this->getConfiguration("Date de départ", "Date à laquelle vous quittez les lieux", ["widget" => "single_text"]))
+            ->add('comment', TextareaType::class, $this->getConfiguration("Commentaire", "Si vous avez un commentaire n'hésitez pas à en faire part !"))
         ;
     }
 
