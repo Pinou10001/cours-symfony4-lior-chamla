963b85e55cced9157cea64a9123b5136d554f704
Martin Soenen <martin.soenen@outlook.com>
2020-05-23 01:29:48 +0200

    142 - Découverte de la notion de Groupes de Validation (ValidationGroups)

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -33,7 +33,7 @@ class Booking
     /**
      * @ORM\Column(type="datetime")
      * @Assert\Date(message="Attention, la date d'arrivée doit être au bon format !")
-     * @Assert\GreaterThan("today", message="La date d'arrivée doit être ultérieure à la date du jour !")
+     * @Assert\GreaterThan("today", message="La date d'arrivée doit être ultérieure à la date du jour !", groups={"front"})
      */
     private $startDate;
 
