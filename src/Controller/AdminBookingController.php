bdf180104727f916f9b6cda7692bf24a6bee14ee
Martin Soenen <martin.soenen@outlook.com>
2020-05-23 01:04:21 +0200

    140 - Découverte du type de champ EntityType pour l'édition d'une réservation

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminBookingController extends AbstractController
@@ -15,6 +18,22 @@ class AdminBookingController extends AbstractController
     {
         return $this->render('admin/booking/index.html.twig', [
             'bookings' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * Permet d'éditer une réservation
+     *
+     * @Route("/admin/bookings/{id}/edit", name="admin_booking_edit")
+     *
+     * @return Response
+     */
+    public function edit(Booking $booking) {
+        $form = $this->createForm(AdminBookingType::class, $booking);
+
+        return $this->render('admin/booking/edit.html.twig', [
+            'form' => $form->createView(),
+            'booking' => $booking
         ]);
     }
 }
