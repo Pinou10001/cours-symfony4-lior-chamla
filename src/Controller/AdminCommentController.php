fb8ef77917ac03f8c69a9db9401b6b3b48943c67
Martin Soenen <martin.soenen@outlook.com>
2020-05-22 17:40:09 +0200

    137 - Exercice : créez le formulaire d'édition d'un commentaire

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Form\EditCommentType;
 use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminCommentController extends AbstractController
@@ -17,4 +23,31 @@ class AdminCommentController extends AbstractController
             'comments' => $repo->findAll(),
         ]);
     }
+
+    /**
+     * Permet d'afficher le formulaire d'édition
+     *
+     * @Route("/admin/comment/{id}/edit", name="admin_comments_edit")
+     *
+     * @param Comment $comment
+     * @return Response
+     */
+    public function edit(Comment $comment, Request $request, EntityManagerInterface $manager) {
+        $form = $this->createForm(EditCommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash('success', "Le commentaire numéro <strong>{$comment->getId()}</strong> a bien été modifié !");
+        }
+
+        return $this->render('admin/comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form->createView()
+        ]);
+    }
+
 }
