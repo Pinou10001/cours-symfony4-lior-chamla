bce6922a362aa126c68a6a8959f5d1265aa9422e
Martin Soenen <martin.soenen@outlook.com>
2020-05-26 20:48:13 +0200

    156 - Les fonctions d'agrégation et le tri

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -18,8 +18,28 @@ class AdminDashboardController extends AbstractController
         $bookings = $manager->createQuery('SELECT count(b) FROM App\Entity\Booking b')->getSingleScalarResult();
         $comments = $manager->createQuery('SELECT count(c) FROM App\Entity\Comment c')->getSingleScalarResult();
 
+        $bestAds = $manager->createQuery(
+            'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
+                  FROM App\Entity\Comment c
+                  JOIN c.ad a
+                  JOIN a.author u
+                  GROUP BY a
+                  ORDER BY note DESC'
+        )->setMaxResults(5)->getResult();
+
+        $worstAds = $manager->createQuery(
+            'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
+                  FROM App\Entity\Comment c
+                  JOIN c.ad a
+                  JOIN a.author u
+                  GROUP BY a
+                  ORDER BY note ASC'
+        )->setMaxResults(5)->getResult();
+
         return $this->render('admin/dashboard/index.html.twig', [
             'stats' => compact('users', 'ads', 'bookings', 'comments'),
+            'bestAds' => $bestAds,
+            'worstAds' => $worstAds
         ]);
     }
 }
