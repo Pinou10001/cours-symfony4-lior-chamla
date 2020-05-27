efc0426d92dbe5e9ceda573edf55048a803a06de
Martin Soenen <martin.soenen@outlook.com>
2020-05-27 21:18:38 +0200

    167 - Créer plusieurs fichiers Javascript pour optimiser les chargements

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/ad', './assets/js/ad.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
     .addStyleEntry('css/app', './assets/css/app.scss')
